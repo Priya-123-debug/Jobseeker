f32d7b4a044d98e6869bdae39e246b79b30e452b
priya-123-debug <supriyakum880@gmail.com>
2026-06-29 09:48:26 +0530

    add

--- server/controller/resumeanalyzercontroller.js
+++ server/controller/resumeanalyzercontroller.js
@@ -110,7 +110,7 @@ Experience Required: ${job.experience} years
 `;
 
     const response = await genAI.models.generateContent({
-      model: "gemini-1.5-flash",
+     model: "gemini-1.5-flash-latest",
       contents: [
         {
           role: "user",
